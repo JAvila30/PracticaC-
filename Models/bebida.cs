6c31a45734972f3a716a5289fda9fbcb26aba46b
MJavila <ajorgetec11l@gmail.com>
2020-11-14 18:59:44 -0500

    First commit

---
+++ Models/bebida.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticasCchar.Models
+{
+    class bebida
+    {
+        public string Nombre { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public bebida(string Nombre, int Cantidad)
+        {
+            this.Nombre = Nombre;
+            this.Cantidad = Cantidad;
+        }
+
+        public void Beberse(int CuantoBebio)
+        {
+            this.Cantidad -= CuantoBebio;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PracticasCchar.Models;
+namespace PracticasCchar
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            bebida bebida = new bebida("Coca cola",1000);
+            bebida.Beberse(500);
+            Console.WriteLine(bebida.Cantidad);
+
+        }
+    }
+}
